21bdc51363340633fa412e373dad7d0b6aff7c76
Daiana <1318176575@qq.com>
2022-07-29 09:23:10 +0800

    实体类添加Test01类

---
+++ src/main/java/com/example/MBG2/entity/Test01.java
@@ -0,0 +1,17 @@
+package com.example.MBG2.entity;
+
+import lombok.Data;
+
+/**
+ * 类功能描述
+ *
+ * @Author 叫我黛安娜
+ * @Date 2022/7/29 9:20
+ */
+@Data
+public class Test01 {
+    private String id;
+    private String name;
+    private String age;
+    private String paper;
+}
