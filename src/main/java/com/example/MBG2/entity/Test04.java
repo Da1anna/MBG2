8866ef5f3166cd5d51f993bae845233aa724ad2b
Daiana <1318176575@qq.com>
2022-07-29 11:19:20 +0800

    实体类添加Test04类

---
+++ src/main/java/com/example/MBG2/entity/Test04.java
@@ -0,0 +1,17 @@
+package com.example.MBG2.entity;
+
+import lombok.Data;
+
+/**
+ * 类功能描述
+ *
+ * @Author 叫我黛安娜
+ * @Date 2022/7/29 11:18
+ */
+@Data
+public class Test04 {
+    private String id;
+    private String name;
+    private String age;
+    private String paper;
+}
