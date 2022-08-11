8beebbae07a8f4f6b4143cca3eb9e4298d6f50e8
Daiana <1318176575@qq.com>
2022-08-11 16:18:19 +0800

    add an entity:Test02

---
+++ src/main/java/com/example/MBG2/entity/Test02.java
@@ -0,0 +1,18 @@
+package com.example.MBG2.entity;
+
+import lombok.Data;
+
+/**
+ * 类功能描述
+ *
+ * @Author 叫我黛安娜
+ * @Date 2022/8/11 16:17
+ */
+@Data
+public class Test02 {
+    private String name;
+    private int age;
+    private String sex;
+    private String address;
+}
+
