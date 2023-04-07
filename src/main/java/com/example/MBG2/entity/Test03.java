9a7ed797e121fc21ad95de6c9a6675258feb2257
Daiana <1318176575@qq.com>
2023-04-07 15:18:03 +0800

    update entity:Test03

--- src/main/java/com/example/MBG2/entity/Test03.java
+++ src/main/java/com/example/MBG2/entity/Test03.java
@@ -1,11 +1,14 @@
 package com.example.MBG2.entity;
 
+import lombok.Data;
+
 /**
  * 类功能描述
  *
  * @Author 叫我黛安娜
  * @Date 2023/4/7 15:09
  */
+@Data
 public class Test03 {
     private String name;
     private String age;
